c6e226d8f1f2608b1c9e620ff3fc07267c26660c
Arturo Murillo Garcia <arturo.murillo.ext@holcim.com>
2025-05-02 17:41:01 -0600

    fix: module

--- tf-modules/glue-database/main.tf
+++ tf-modules/glue-database/main.tf
@@ -1,34 +1,32 @@
-#######################
-## AWS Glue Database ##
-#######################
-module "glue_db_label" {
-  source = "git::git@gitlab.com:holcim-adc/americas-core/tools/tf-modules.git//?ref=null/tagging_0.3.0"
-  name   = var.db_name
-
-  context = module.this.context
-}
+# Elimina la referencia al módulo "this"
+# y usa un enfoque más simple
 
 locals {
-  database_name = var.custom_db_name ? var.db_name : "${module.glue_db_label.id}-db"
+  database_name = var.custom_db_name ? var.db_name : "${var.db_name}-db"
+  tags = merge(
+    {
+      Name = local.database_name,
+      Environment = var.environment
+    },
+    var.tags
+  )
 }
 
 resource "aws_glue_catalog_database" "this" {
   name         = local.database_name
-  description  = var.db_description
+  description  = var.description
   location_uri = var.location_uri
-  tags         = merge(module.glue_db_label.tags, { Name = local.database_name })
+  tags         = local.tags
 }
 
-#############################
-## AWS Lake Formation Tags ##
-#############################
+# Para las etiquetas LF, simplifica para usar directamente las variables proporcionadas
 resource "aws_lakeformation_resource_lf_tags" "this" {
   database {
     name = aws_glue_catalog_database.this.name
   }
 
   dynamic "lf_tag" {
-    for_each = merge({ project1 : module.this.tags.projects1 }, var.lf_tags)
+    for_each = var.lf_tags
     content {
       key   = lf_tag.key
       value = lf_tag.value
--- tf-modules/glue-database/variables.tf
+++ tf-modules/glue-database/variables.tf
@@ -1,28 +1,40 @@
+variable "db_name" {
+  description = "Name of the glue database"
+  type        = string
+}
+
+variable "description" {
+  description = "Description of the glue database"
+  type        = string
+  default     = ""
+}
+
 variable "custom_db_name" {
+  description = "Use the db_name as is without any modifications"
   type        = bool
-  description = "Whether to use or not a custom name for the Glue DB. Meant for backwards compatibility."
   default     = false
 }
 
-variable "db_name" {
-  type        = string
-  description = "Name to be used for the Glue DB. Will be a suffix if `custom_db_name` is true."
-}
-
-variable "db_description" {
-  type        = string
-  description = "Glue DB description."
-  default     = null
-}
-
 variable "location_uri" {
+  description = "Location URI for the database"
   type        = string
-  description = "Location of the data store associated with the Glue DB."
   default     = null
+}
+
+variable "environment" {
+  description = "Environment for resource tagging"
+  type        = string
+  default     = "dev"
+}
+
+variable "tags" {
+  description = "Additional tags for the database"
+  type        = map(string)
+  default     = {}
 }
 
 variable "lf_tags" {
+  description = "Lake Formation tags to apply to the database"
   type        = map(string)
-  description = "Extra LF taggs to associate with the Glue DB."
   default     = {}
 }
