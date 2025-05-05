525529812b9514139d00cc1b11a8060da1c6d210
Arturo Murillo Garcia <arturo.murillo.ext@holcim.com>
2025-05-05 12:17:46 -0600

    feat: bedrock

--- modules/aws/bedrock/main.tf
+++ modules/aws/bedrock/main.tf
@@ -119,14 +119,18 @@ resource "aws_iam_role_policy" "bedrock_kb_opensearch_access" {
   })
 }
 
-# Wait for the role policy to be attached/propagated to the role before creating the collection
-resource "time_sleep" "wait_for_policy_propagation" {
+resource "time_sleep" "wait_for_new_role_policy" {
+  count           = var.create_iam_role ? 1 : 0
   create_duration = "20s"
-  depends_on = [
-    aws_iam_role_policy.bedrock_kb_policy[*],  # Use splat operator to handle count
-    aws_iam_role_policy.bedrock_kb_opensearch_access[*]
-  ]
+  depends_on      = [aws_iam_role_policy.bedrock_kb_policy]
 }
+
+resource "time_sleep" "wait_for_existing_role_policy" {
+  count           = var.create_iam_role ? 0 : 1
+  create_duration = "20s"
+  depends_on      = [aws_iam_role_policy.bedrock_kb_opensearch_access]
+}
+
 # Note that the healthcheck argument is set to false because the
 #client health check does not really work with OpenSearch Serverless.
 provider "opensearch" {
