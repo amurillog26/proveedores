fd891775e3f417220e9f8d8b99ed780188501096
Arturo Murillo Garcia <arturo.murillo.ext@holcim.com>
2025-05-12 17:33:18 -0600

    feat: bedrock update

--- modules/aws/bedrock/outputs.tf
+++ modules/aws/bedrock/outputs.tf
@@ -24,11 +24,6 @@ output "kb_association_id" {
   value       = var.create_agent ? aws_bedrockagent_agent_knowledge_base_association.this[0].id : null
 }
 
-output "agent_alias_id" {
-  description = "ID of the agent alias"
-  value       = var.create_agent && var.create_agent_version && var.create_agent_alias ? aws_bedrockagent_agent_alias.production[0].agent_alias_id : null
-}
-
 output "agent_action_group_id" {
   description = "ID of the agent action group"
   value       = var.create_agent ? aws_bedrockagent_agent_action_group.p2p_functions[0].id : null
